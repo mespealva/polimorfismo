328c7de44d05136d4d35cb8065af5825fb59bf11
maria alvarez <mar.alvareg@gmail.com>
2020-09-13 16:56:07 -0300

    initial

---
+++ ejercicio1.rb
@@ -0,0 +1,40 @@
+class Person
+    def initialize(first, last, age)
+        @first_name = first
+        @last_name = last
+        @age = age
+    end
+    def birthday
+        @age += 1
+    end
+end
+
+class Student < Person
+    def talk
+        "Aquí es la clase de programación con Ruby?" 
+    end
+    def introduce
+        "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+class Teacher < Person
+    def talk
+        "Bienvenidos a la clase de programación con Ruby!"
+    end
+    def introduce
+        "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+class Parent < Person
+    def talk
+        "Aquí es la reunión de apoderados?"
+    end
+    def introduce
+        "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+p1 = Parent.new('Jose', 'Ramos', 31)
+puts p1.introduce
---
+++ ejercicio2.rb
@@ -0,0 +1,106 @@
+module Habilidades
+    module Volador
+        def volar
+            'Estoy volandooooo!'
+        end
+        def aterrizar
+            'Estoy cansado de volar, voy a aterrizar'
+        end
+    end
+    module Nadador
+        def nadar
+            'Estoy nadando!'
+        end
+        def sumergir
+            'glu glub glub glu'
+        end
+    end
+    module Caminante
+        def caminar
+            'Puedo caminar!'
+        end
+    end
+end
+
+module Alimentacion
+    module Herbivoro
+        def comer
+            'Puedo comer plantas!'
+        end
+    end
+    module Carnivoro
+        def comer
+            'Puedo comer carne!'
+        end
+    end
+end
+
+class Animal
+    attr_reader :nombre
+    def initialize(nombre)
+        @nombre = nombre
+    end    
+end
+
+class Ave < Animal  
+end
+
+class Mamifero < Animal 
+end
+
+class Insecto
+end
+
+class Pinguino < Ave
+    include Habilidades::Nadador 
+    include Habilidades::Caminante
+    include Alimentacion::Carnivoro
+end
+
+class Paloma < Ave
+    include Habilidades::Volador
+    include Habilidades::Caminante
+    include Alimentacion::Carnivoro
+end
+
+class Pato < Ave
+    include Habilidades::Volador
+    include Habilidades::Nadador
+    include Habilidades::Caminante
+    include Alimentacion::Herbivoro
+    include Alimentacion::Carnivoro
+end
+
+class Perro < Mamifero
+    include Habilidades::Caminante
+    include Alimentacion::Carnivoro
+end
+
+class Gato < Mamifero
+    include Habilidades::Caminante
+    include Alimentacion::Carnivoro
+end
+
+class Vaca < Mamifero
+    include Habilidades::Caminante
+    include Alimentacion::Herbivoro
+end
+
+class Mosca < Insecto
+    include Habilidades::Volador
+    include Habilidades::Caminante
+    include Alimentacion::Carnivoro
+    include Alimentacion::Herbivoro
+end
+
+class Mariposa < Insecto
+    include Habilidades::Volador 
+    include Habilidades::Caminante
+    include Alimentacion::Herbivoro
+end
+
+class Abeja < Insecto
+    include Habilidades::Volador
+    include Habilidades::Caminante
+    include Alimentacion::Herbivoro
+end
